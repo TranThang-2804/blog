ce636e8d630c8ed68de2181c71888fd0b7cd96d9
Tommy Tran Duc Thang <tranthang.dev@gmail.com>
2024-11-01 19:37:59 +0900

    feat: customizing for ghpage

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
+  website: "https://TranThang-2804.github.io/", // replace this with your deployed domain
+  author: "Tommy Tran Duc Thang",
+  profile: "https://tommytran.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "AstroPaper",
   ogImage: "astropaper-og.jpg",
